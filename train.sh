d6cad2fa40fe60f611c64c0e3527d8a03907edb8
dq <lizihan@umich.edu>
2017-07-19 16:06:43 -0700

    changed train.sh

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python tools/train.py avg sts datasets/all/2015.train.tsv datasets/all/2015.val.tsv inp_e_dropout=1/2 nb_epoch=64
+python tools/train.py avg sts data/all/2015.train.tsv data/all/2015.val.tsv inp_e_dropout=1/2 nb_epoch=64
