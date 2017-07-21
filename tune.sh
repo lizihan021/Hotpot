e6475da7acd985a7130828721e5f938a7756b217
lizihan <lizihan@umich.edu>
2017-07-20 17:13:16 -0700

    added tuned

--- tune.sh
+++ tune.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
-python3 tools/tuning.py cnn anssel data/anssel/wang/train-all.csv data/anssel/wang/dev.csv \
-        "dropout=[1/2, 2/3, 3/4]" "inp_e_dropout=[1/2, 3/4, 4/5]" "l2reg=[1e-4, 1e-3, 1e-2]" \
-        "project=[True, True, False]" "cnnact=['tanh', 'relu']" \
-        "cdim={1: [0,0,1/2,1,2], 2: [0,0,1/2,1,2,0], 3: [0,0,1/2,1,2,0], 4: [0,0,1/2,1,2,0], 5: [0,0,1/2,1,2]},"
+python3 tools/tuning.py avg sts data/all/2015.train.tsv data/all/2015.val.tsv \
+        "dropout=[1/2, 2/3, 3/4]" "inp_e_dropout=[1/2, 3/4, 4/5]" "l2reg=[1e-4, 1e-3, 1e-2]"
