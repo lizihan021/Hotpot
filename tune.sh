213d97a9c9a9cde9c83c12852a4e4181ce120d49
lizihan <lizihan@umich.edu>
2017-07-20 17:07:54 -0700

    blocks

--- tune.sh
+++ tune.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
-python tools/train.py avg sts data/all/2015.train.tsv \
-data/all/2015.val.tsv inp_e_dropout=1/2 epochs=64
+python3 tools/tuning.py cnn anssel data/anssel/wang/train-all.csv data/anssel/wang/dev.csv \
+        "dropout=[1/2, 2/3, 3/4]" "inp_e_dropout=[1/2, 3/4, 4/5]" "l2reg=[1e-4, 1e-3, 1e-2]" \
+        "project=[True, True, False]" "cnnact=['tanh', 'relu']" \
+        "cdim={1: [0,0,1/2,1,2], 2: [0,0,1/2,1,2,0], 3: [0,0,1/2,1,2,0], 4: [0,0,1/2,1,2,0], 5: [0,0,1/2,1,2]},"
