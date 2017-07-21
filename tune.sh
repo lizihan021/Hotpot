813164869ba8a05b9172e038c9abd11ff09e7a18
lizihan <lizihan@umich.edu>
2017-07-21 15:48:13 -0700

    tune avg 2

--- tune.sh
+++ tune.sh
@@ -3,7 +3,7 @@ python3 tools/tuning.py avg sts data/all/2015.train.tsv data/all/2015.val.tsv \
 		"embprune=[100, 200]" "embicase=[True, False]" "inp_e_dropout=[1/3, 1/2, 0.65, 0.8]" \
 		"inp_w_dropout=[1/3, 1/2]" "e_add_flags=[True, False]"
 python3 tools/tuning.py avg sts data/all/2015.train.tsv data/all/2015.val.tsv \
-		"inp_e_dropout=[3/4, 4/5]" "Ddim=[1, 2]"
+		"mlpsum=[sum, absdiff]" "Ddim=[1, 2]"
 python3 tools/tuning.py avg sts data/all/2015.train.tsv data/all/2015.val.tsv \
 		"batch_size=[80, 160, 320]"
 python3 tools/tuning.py avg sts data/all/2015.train.tsv data/all/2015.val.tsv \
