4f20f9207a99981507834d229864302949f59866
lizihan <lizihan@umich.edu>
2017-07-21 10:25:43 -0700

    test tuning

--- tune.sh
+++ tune.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 python3 tools/tuning.py avg sts data/all/2015.train.tsv data/all/2015.val.tsv \
-		"inp_e_dropout=[1/2, 3/4, 4/5]" "l2reg=[1e-4, 1e-3, 1e-2]"
+		"inp_e_dropout=[3/4, 4/5]" "l2reg=[1e-3, 1e-2]"
