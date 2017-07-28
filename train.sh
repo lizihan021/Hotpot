4696eedb177555eb36e55adc98a904f1fbb00e92
LiangKlausQiu <liangqiu@outlook.com>
2017-07-28 14:42:41 -0700

    yo

--- train.sh
+++ train.sh
@@ -2,5 +2,5 @@
 #python3 tools/train.py dnn sts data/all/2015.train.tsv \
 #data/all/2015.val.tsv
 
-python3 tools/train.py avg para data/question-pairs-dataset/questions.csv \
-data/question-pairs-dataset/questions.csv
+python3 tools/train.py avg para data/question-pairs-dataset/train.csv \
+data/question-pairs-dataset/test.csv
