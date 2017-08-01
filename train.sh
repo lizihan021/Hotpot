23705459eaeb3631769144195dd7dd276d450520
lizihan <lizihan@umich.edu>
2017-08-01 14:10:03 -0700

    yo

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-python3 tools/train.py avg sts data/all/2015.train.tsv \
-data/all/2015.val.tsv
+# python3 tools/train.py avg sts data/all/2015.train.tsv \
+# data/all/2015.val.tsv
 
-#python3 tools/train.py avg para data/question-pairs-dataset/train.csv \
-#data/question-pairs-dataset/test.csv batch_size=1
+python3 tools/train.py gran para data/question-pairs-dataset/train-small.csv \
+data/question-pairs-dataset/test.csv
