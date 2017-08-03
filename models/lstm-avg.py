494e8b95467627bfbc94e05a2cc2a52f07910157
lizihan <lizihan@umich.edu>
2017-08-03 13:37:42 -0700

    yo

--- models/lstm-avg.py
+++ models/lstm-avg.py
@@ -1,29 +1,3 @@
-"""
-A simple averaging model.
-
-In its default settings, this is the baseline unigram (Yu, 2014) approach
-http://arxiv.org/abs/1412.1632 of training (M, b) such that:
-
-    f(q, a) = sigmoid(q * M * a.T + b)
-
-However, rather than a dot-product, the MLP comparison is used as it works
-dramatically better.
-
-This model can also represent the Deep Averaging Networks
-(http://cs.umd.edu/~miyyer/pubs/2015_acl_dan.pdf) with this configuration:
-
-    inp_e_dropout=0 inp_w_dropout=1/3 deep=2 "pact='relu'"
-
-The model also supports preprojection of embeddings (not done by default;
-wproj=True), though it doesn't do a lot of good it seems - the idea was to
-allow mixin of NLP flags.
-
-
-Performance:
-    * anssel-yodaqa:
-      valMRR=0.334864 (dot)
-"""
-
 from __future__ import print_function
 from __future__ import division
 
@@ -36,19 +10,6 @@ import pysts.kerasts.blocks as B
 
 def config(c):
     c['l2reg'] = 1e-5
-
-    # word-level projection before averaging
-    c['wproject'] = True
-    c['wdim'] = 1
-    c['wact'] = 'linear'
-
-    c['deep'] = 0
-    c['nnact'] = 'relu'
-    c['nninit'] = 'glorot_uniform'
-
-    c['project'] = False
-    c['pdim'] = 1
-    c['pact'] = 'tanh'
 
     # model-external:
     c['inp_e_dropout'] = 1/3
