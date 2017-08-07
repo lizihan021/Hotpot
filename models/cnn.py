668d108458f35086dd8cd2da4328a080c52ce9eb
Yuanyi Ding <semiswiet@gmail.com>
2017-08-07 13:56:02 -0700

    fix comment

--- models/cnn.py
+++ models/cnn.py
@@ -12,9 +12,8 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-"""
 
-"""
+
 A model with a very simple architecture that never-the-less achieves
 2015-state-of-art results on the anssel-wang task (with token flags).
 You can also see this model as a standalone fully contained script
